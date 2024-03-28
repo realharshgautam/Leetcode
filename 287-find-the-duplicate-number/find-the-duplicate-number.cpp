2058fde23facff5945eb11c1c5f2c9fa9251e6a4
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-03-28 06:05:14 +0530

    Time: 93 ms (42.71%) | Memory: 63.5 MB (93.29%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+    int findDuplicate(std::vector<int>& nums) {
+        int left = 1;
+        int right = nums.size() - 1;
 
-        for(int i =0; i<nums.size()-1 ;i++){
-            if(nums[i] == nums[i+1]){
-            return nums[i];
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+
+            // Count the numbers less than or equal to mid
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+
+            // If count is greater than mid, the duplicate lies in the left half
+            if (count > mid) {
+                right = mid;
+            } else { // Otherwise, it lies in the right half
+                left = mid + 1;
             }
         }
-        return -1;
-        
+
+        return left;
     }
 };
