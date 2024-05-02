0942808fcd6eee240a2be6a20290ee1160a75c23
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-05-02 20:23:56 +0530

    Time: 24 ms (33.88%) | Memory: 23 MB (52.66%) - LeetSync

---
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findMaxK(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = n-1; i >= 0; i--) {
+            if (nums[i] > 0 && std::binary_search(nums.begin(), nums.end(), -nums[i])) {
+                return nums[i];
+            }
+        }
+        return -1;  // If no such pair found
+    }
+};
