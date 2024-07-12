bba82879a7302f916f1a3d8b6bcec78c6de2f81e
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-07-12 13:53:36 +0530

    Time: 48 ms (97.97%) | Memory: 18.2 MB (75.61%) - LeetSync

---
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maximumGain(string s, int x, int y) {
+        int aCount = 0;
+        int bCount = 0;
+        int lesser = min(x, y);
+        int result = 0;
+
+        for (char c : s) {
+            if (c > 'b') {
+                result += min(aCount, bCount) * lesser;
+                aCount = 0;
+                bCount = 0;
+            } else if (c == 'a') {
+                if (x < y && bCount > 0) {
+                    bCount--;
+                    result += y;
+                } else {
+                    aCount++;
+                }
+            } else {
+                if (x > y && aCount > 0) {
+                    aCount--;
+                    result += x;
+                } else {
+                    bCount++;
+                }
+            }
+        }
+
+        result += min(aCount, bCount) * lesser;
+        return result;
+    }
+};
