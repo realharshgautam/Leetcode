fa45faab0439eb2055e9022f55e662eaca197180
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-07-20 23:25:43 +0530

    Time: 37 ms (60.27%) | Memory: 36.2 MB (11.51%) - LeetSync

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int rows = rowSum.size();
+        int cols = colSum.size();
+        int cur_row = 0, cur_col = 0;
+        vector<vector<int>> res(rows, vector<int>(cols, 0));
+
+        while (cur_row < rows || cur_col < cols) {
+            if (cur_row >= rows) {
+                res[rows - 1][cur_col] = colSum[cur_col];
+                cur_col++;
+                continue;
+            } else if (cur_col >= cols) {
+                res[cur_row][cols - 1] = rowSum[cur_row];
+                cur_row++;
+                continue;
+            }
+
+            int value_to_put = min(rowSum[cur_row], colSum[cur_col]);
+            rowSum[cur_row] -= value_to_put;
+            colSum[cur_col] -= value_to_put;
+            res[cur_row][cur_col] = value_to_put;
+
+            // I write this as this because it's possible that rowSum[cur_row] == colSum[cur_col] and we'll want to move both row and col pointers
+            if (rowSum[cur_row] == 0) {
+                cur_row++;
+            }
+            if (colSum[cur_col] == 0) {
+                cur_col++;
+            }
+        }
+        return res;
+    }
+};
