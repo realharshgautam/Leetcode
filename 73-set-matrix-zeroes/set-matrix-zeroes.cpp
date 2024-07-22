8375ff1fc7589fa5bf82029ed84edd4e087f5620
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-07-22 19:55:20 +0530

    Time: 12 ms (39.72%) | Memory: 16.8 MB (20.18%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,26 +3,36 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<int> row(m, 0);
-        vector<int> col(n, 0);
-
+        // vector<int> row(m, 0); matrix [..][0]
+        // vector<int> col(n, 0); matrix [0][..]
+        int col0 =1;
         // Find zero elements and mark the corresponding row and column
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
                 if(matrix[i][j] == 0) {
-                    row[i] = 1;
-                    col[j] = 1;
+                    // mark the ith row[i] = 0;
+                    matrix[i][0]=0;
+                    if(j!=0)
+                        matrix[0][j]=0;
+                    else
+                        col0 =0;
                 }
             }
         }
 
         // Set matrix elements to zero based on the row and column markers
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(row[i] == 1 || col[j] == 1) {
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[0][j] == 0 || matrix[i][0] == 0) {
                     matrix[i][j] = 0;
                 }
             }
+        }
+        if(matrix[0][0]==0){
+            for(int j=0; j<n;j++) matrix[0][j]=0;
+        }
+        if(col0 ==0){
+            for(int i=0; i<m;i++) matrix[i][0]=0;
         }
     }
 };
