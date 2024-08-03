218837adfb7f5990480d6111baa1bebb46b709c2
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-08-03 19:57:34 +0530

    Time: 44 ms (99.61%) | Memory: 83.3 MB (45.26%) - LeetSync

--- 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,14 +1,40 @@
+static const int fast = []{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+
+    return 0;
+}();
+
 class Solution {
 public:
     int minSwaps(vector<int>& nums) {
-        int k = accumulate(nums.begin(), nums.end(), 0);
         int n = nums.size();
-        int cnt = accumulate(nums.begin(), nums.begin() + k, 0);
-        int mx = cnt;
-        for (int i = k; i < n + k; ++i) {
-            cnt += nums[i % n] - nums[(i - k + n) % n];
-            mx = max(mx, cnt);
+
+        int cnt = 0;
+        for(int& x : nums)  cnt += x;
+
+        if(cnt == n)    return 0;
+
+        int swaps = n;
+
+        int l = 0;
+        int r = 0;
+        int curr = 0;
+
+        while(l < n)
+        {
+            while(r >= l ? r - l < cnt : n - l + r < cnt)
+            {
+                curr += nums[r];
+                r = (r + 1) % n;
+            }
+
+            swaps = min(swaps, cnt - curr);
+            curr -= nums[l];
+            l++;
         }
-        return k - mx;
+
+        return swaps;
     }
 };
