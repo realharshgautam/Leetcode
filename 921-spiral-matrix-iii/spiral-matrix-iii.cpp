2d5b7b4b2ebb55bc3fccbc1a36867809049bf1e4
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2024-08-08 23:41:36 +0530

    Time: 7 ms (86.90%) | Memory: 13.7 MB (36.78%) - LeetSync

---
+++ 921-spiral-matrix-iii/spiral-matrix-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int i = rStart, j = cStart;
+        int diri = 0, dirj = 1; // directions to move
+        int twice = 2;
+        vector<vector<int>> res;
+        int moves = 1;
+        int next_moves = 2;
+
+        while (res.size() < rows * cols) {
+            if (i >= 0 && i < rows && j >= 0 && j < cols) {
+                res.push_back({i, j});
+            }
+
+            i += diri;
+            j += dirj;
+            moves -= 1;
+            if (moves == 0) {
+                int temp = diri;
+                diri = dirj;
+                dirj = -temp; // right direction
+                twice -= 1;
+                if (twice == 0) {
+                    twice = 2;
+                    moves = next_moves;
+                    next_moves += 1;
+                } else {
+                    moves = next_moves - 1;
+                }
+            }
+        }
+        return res;
+    }
+};
