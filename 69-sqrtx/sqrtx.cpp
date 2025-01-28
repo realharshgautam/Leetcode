4cd21730b156a12f75f3e5a86294d4164be22ee0
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2025-01-28 19:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (15.29%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low =0 , high = x;
+      long ans = 0;
+      while(low<=high){
+        long long mid = low + (high - low)/2;
+        long long val = mid*mid; 
+        if(val<=x){
+            ans = mid;
+            low=mid+1;
+        }
+        else{
+        high = mid-1; }
+      }
+      return high;
+    }
+};
