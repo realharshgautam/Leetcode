69aaff6284240b4e7cf629f2a0bf76c1053f74e9
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2025-01-30 10:51:52 +0530

    Time: 16 ms (20.90%) | Memory: 26.2 MB (49.96%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -14,14 +14,16 @@ class Solution {
         if (n > limit) return -1;
 
         int low = 1, high = *max_element(nums.begin(), nums.end());
+        int ans=high;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
             if (sumByD(nums, mid) <= limit) {
+                ans=mid;
                 high = mid - 1;
             } else
                 low = mid + 1;
         }
-        return low;
+        return ans;
     }
 };
