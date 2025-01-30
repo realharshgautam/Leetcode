34b3c39fbef3f782f41b57585098a69bb64d75c1
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2025-01-30 11:35:38 +0530

    Time: 6 ms (80.13%) | Memory: 39.6 MB (5.02%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -11,9 +11,20 @@ long long reqHours(vector<int>nums, int hours){
         return sum;
     }
 
+int findMax(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int n = nums.size();
+        
+        // Find the maximum element
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+        }
+        return maxi;
+    }
+
 int minEatingSpeed(vector<int>&nums, int h) {
     int low =1; 
-    int high = *max_element(nums.begin(), nums.end());
+    int high = findMax(nums);
     while(low<=high){
         long long mid = (low+high)/2;
         long long reqTime = reqHours(nums,mid);
