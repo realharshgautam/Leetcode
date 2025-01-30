92d05c349265eefe5bdca36be080d74567cf5748
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2025-01-30 10:49:52 +0530

    Time: 11 ms (53.06%) | Memory: 26.2 MB (49.96%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   private:
+    int sumByD(vector<int> &nums, int div) {
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            sum += ceil((double)nums[i] / (double)div);
+        }
+        return sum;
+    }
+
+   public:
+    int smallestDivisor(vector<int> &nums, int limit) {
+        int n = nums.size();
+        if (n > limit) return -1;
+
+        int low = 1, high = *max_element(nums.begin(), nums.end());
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (sumByD(nums, mid) <= limit) {
+                high = mid - 1;
+            } else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
