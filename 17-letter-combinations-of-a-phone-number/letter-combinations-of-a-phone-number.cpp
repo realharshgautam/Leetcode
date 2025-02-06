5c2d288e1b09e0e703d32a8ac93479e24fe3822d
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2025-02-07 04:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (98.81%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+    // Recursive function to generate letter combinations
+    void func(int ind, const string& digits, string s, vector<string>& ans,
+              const string combos[]) {
+        // Base case: if index reaches the end of digits, add the current
+        // combination
+        if (ind == digits.size()) {
+            ans.push_back(s);
+            return;
+        }
+        // Convert the current character to an integer (digit)
+        int digit = digits[ind] - '0';
+        // Loop through the corresponding characters for the current digit
+        for (int i = 0; i < combos[digit].size(); i++) {
+            // Recursively build the combination by adding the current letter
+            func(ind + 1, digits, s + combos[digit][i], ans, combos);
+        }
+    }
+
+public:
+    vector<string> letterCombinations(string digits) {
+        // Return an empty vector if the input is empty
+        if (digits.empty()) {
+            return {};
+        }
+
+        // Mapping digits to corresponding characters (index 0 and 1 are empty)
+        string combos[] = {"",    "",    "abc",  "def", "ghi",
+                           "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> ans; // Vector to store the result
+        string s = "";      // Temporary string to build combinations
+
+        // Initiate recursive function starting from index 0
+        func(0, digits, s, ans, combos);
+        return ans; // Return the resulting combinations
+    }
+};
