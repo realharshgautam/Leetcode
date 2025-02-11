192840ebc2be5e583751d60fa5454ae89058aff6
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2025-02-12 01:43:13 +0530

    Time: 195 ms (86.63%) | Memory: 10.3 MB (95.05%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,33 +1,32 @@
 class Solution {
-public:
-    bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
+   private:
+    bool func(vector<vector<char> >& v, int i, int j, string& s, int k) {
+        if (k == s.length()) {
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= v.size() || j >= v[0].size() ||
+            s[k] != v[i][j]) {
             return false;
-        };
-        
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
-                    return true;
+        }
+        bool ans = false;
+        char x = v[i][j];
+        v[i][j] = ' ';
+        ans |= func(v, i + 1, j, s, k + 1);
+        ans |= func(v, i - 1, j, s, k + 1);
+        ans |= func(v, i, j + 1, s, k + 1);
+        ans |= func(v, i, j - 1, s, k + 1);
+        v[i][j] = x;
+        return ans;
+    }
+
+   public:
+    bool exist(vector<vector<char> >& board, string word) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == word[0]) {
+                    if (func(board, i, j, word, 0)) {
+                        return true;
+                    }
                 }
             }
         }
