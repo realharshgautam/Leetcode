45e1a2c07b4c2105d35e2b60eb5c7860286b69f9
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2025-02-13 22:09:28 +0530

    Time: 110 ms (13.26%) | Memory: 91.4 MB (90.45%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-    priority_queue<long long, vector<long long>, greater<long long>> pq;
-    for(auto n : nums) pq.push(n); 
-    int count = 0;
-    for(;pq.top() < k; ++count){
-        long minNum = pq.top();       pq.pop();
-        pq.push(2*minNum + pq.top()); pq.pop();
+        priority_queue<long, vector<long>, greater<long>> min_heap(nums.begin(),
+                                                                   nums.end());
+        int num_operations = 0;
+
+        while (min_heap.top() < k) {
+            long x = min_heap.top();
+            min_heap.pop();
+            long y = min_heap.top();
+            min_heap.pop();
+            min_heap.push(min(x, y) * 2 + max(x, y));
+
+            num_operations++;
+        }
+        return num_operations;
     }
-    return count;
-}
 };
