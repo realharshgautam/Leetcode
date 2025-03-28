52ace3c3e82537e5d5733a1d3a674d2eb4f42f48
Harsh Gautam <93820223+realharshgautam@users.noreply.github.com>
2025-03-28 19:51:17 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (87.54%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+       int n = ratings.size();
+        int totalCandies = n;
+        int i = 1;
+
+        while (i < n) {
+            if (ratings[i] == ratings[i - 1]) {
+                i++;
+                continue;
+            }
+
+            int currentPeak = 0;
+            while (i < n && ratings[i] > ratings[i - 1]) {
+                currentPeak++;
+                totalCandies += currentPeak;
+                i++;
+            }
+
+            if (i == n) {
+                return totalCandies;
+            }
+
+            int currentValley = 0;
+            while (i < n && ratings[i] < ratings[i - 1]) {
+                currentValley++;
+                totalCandies += currentValley;
+                i++;
+            }
+
+            totalCandies -= min(currentPeak, currentValley);
+        }
+
+        return totalCandies;        
+    }
+};
